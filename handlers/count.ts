3933b95d393622e16883babdb7b36e2a818750cb
yourname <yourname@example.com>
2024-12-15 19:39:56 +0800

    FX

--- handlers/count.ts
+++ handlers/count.ts
@@ -3,6 +3,6 @@ import { sqlite } from '../db';
 export default async () => {
     const sql = `SELECT COUNT(*) AS value FROM verification_code`
     const query = sqlite.query(sql)
-    const result = query.all();
-    return result[0].value
+    const result = query.get();
+    return result.value
 }
