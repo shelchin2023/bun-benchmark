9f831bc6715bdbd801773737939f04f6af7deb33
yourname <yourname@example.com>
2024-12-15 16:41:36 +0800

    fx

--- index.ts
+++
@@ -1,9 +0,0 @@
-const port = 6891
-Bun.serve({
-    port: port,
-    fetch(req) {
-        // console.log(req)
-        return new Response("Bun!");
-    },
-});
-console.log(`http://localhost:${port}`)
