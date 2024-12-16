b3fa34d41fa9a6cd5fc179adeb81879f13ffaec5
yourname <yourname@example.com>
2024-12-16 13:23:42 +0800

    fx

--- db.ts
+++ db.ts
@@ -3,6 +3,9 @@ import { Database } from 'bun:sqlite';
 
 
 const sqlite = new Database('sqlite.db');
+sqlite.run('pragma busy_timeout = 5000;');
+sqlite.run('PRAGMA journal_mode=WAL;');
+
 const db = drizzle(sqlite);
 
-export { db ,sqlite};
+export { db, sqlite };
